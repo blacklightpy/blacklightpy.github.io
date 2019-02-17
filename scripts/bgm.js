4f8792bafa12f7d4fd15f150bb3d10abe644f3b7
Jyothish Kumar M S <jeevanms15@gmail.com>
2019-02-18 03:02:11 +0530

    Update bgm.js

--- scripts/bgm.js
+++ scripts/bgm.js
@@ -1,19 +1,36 @@
 var isChromiumBasedBrowser = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
 var bgplayer;
 if(isChromiumBasedBrowser){
-        bgplayer = document.createElement("iframe");
-        bgplayer.setAttribute("src", "/music/intro.mp3");
-        bgplayer.setAttribute("allow", "autoplay");
-        bgplayer.setAttribute("style", "display:none");
+  bgplayer = document.createElement("iframe");
+  bgplayer.setAttribute("src", "/music/intro.mp3");
+  bgplayer.setAttribute("allow", "autoplay");
+  bgplayer.setAttribute("style", "display:none");
 }
 else{
-        bgplayer = document.createElement("audio");
-        bgplayer.setAttribute("autoplay", "autoplay");
-        bgplayer.setAttribute("loop", "loop");
-        var src = document.createElement("source");
-        src.setAttribute("src", "/music/intro.mp3");
-        src.setAttribute("type", "audio/mpeg");
-        bgplayer.appendChild(src);
+  bgplayer = document.createElement("audio");
+  bgplayer.setAttribute("autoplay", "autoplay");
+  bgplayer.setAttribute("loop", "loop");
+  var src = document.createElement("source");
+  src.setAttribute("src", "/music/intro.mp3");
+  src.setAttribute("type", "audio/mpeg");
+  bgplayer.appendChild(src);
 }
 bgplayer.setAttribute("id", "bgm");
 document.body.appendChild(bgplayer);
+
+function toggle(){ 
+  if (bgplayer.paused){
+    if(isChromiumBasedBrowser)
+      $("#bgm").attr("src", "/music/intro.mp3");
+    else
+      $("#bgm").play();            
+    $("#toggle").text('Pause');
+  }
+  else{
+    if(isChromiumBasedBrowser)
+      $("#bgm").removeAttr("src");
+    else
+      $("#bgm").pause(); 
+    $("#toggle").text('Play');
+  }
+}
